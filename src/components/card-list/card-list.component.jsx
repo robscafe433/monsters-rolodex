f86e8117a826e32856a26611a6f22d2bbe20abe0
petervil <petervil@protonmail.com>
2022-07-28 23:09:52 +0200

    adding more style to page / components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-
+import "./card-list.styles.css";
 class CardList extends Component {
   render() {
     console.log(this.props.monsters);
@@ -11,9 +11,19 @@ class CardList extends Component {
 
     return (
       <div className='card-list'>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className='card-container' key={id}>
+              <img
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                alt={`monster ${name}`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
