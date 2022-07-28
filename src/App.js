36463cba3ad4dc7874cb2cfbce3907d5ff760ec9
petervil <petervil@protonmail.com>
2022-07-28 13:17:28 +0200

    app optimization

--- src/App.js
+++ src/App.js
@@ -13,15 +13,41 @@ class App extends Component {
     this.state = {
       // null state ... no users -> showing empty array in case of error
       monsters: [],
+      // adding searchField to our state
       searchField: "",
     };
     console.log("Constructor");
   }
+  // ===================================================
   // WE need to answer following questions:
   // 1) How to get the monster list?
   // the list should rerender as soon as app is mounted (started)
   // A : using React lifecycle methods .
   // ORDER : componentDidMount runs THIRD (after render function)
+  // 2) Where to get the list from?
+  // we need to fetch it from API
+  // https://jsonplaceholder.typicode.com/users
+  // 3) Where to put the list?
+  // A: gonna be in our state monster array
+
+  // ======================================================
+  // onSearchChange optimization, so this method is not reinitialize every time rerender happens
+  onSearchChange = (event) => {
+    // console.log({ startingArray: this.state.monsters });
+    // lowerCase the search
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    // updates our state
+    this.setState(
+      () => {
+        return { searchField };
+      },
+      () => {
+        // console.log({ endingArray: this.state.monsters });
+      }
+    );
+  };
+
   componentDidMount() {
     console.log("Component Mount");
     // we gonna fetch the data from this API
@@ -48,29 +74,19 @@ class App extends Component {
         )
       );
   }
-  // ===================================================
-  // 2) Where to get the list from?
-  // we need to fetch it from API
-  // https://jsonplaceholder.typicode.com/users
-  // 3) Where to put the list?
-  // A: gonna be in our state monster array
-  // .then((users) =>
-  // this.setState(
-  //   () => {
-  //     return { monsters: users };
-  //   });
-  // ======================================================
 
   // ORDER: render function runs after construction function
-  // it determines what to show
+  // it determines what to show and it runs every time React needs to update DOM
   render() {
     console.log("Render");
 
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
     // we need to filter monsters to match from searchbox
     // we know that monsters = [{'name: Leanne'}, {name : 'Ervin'}] etc ..
-    const filteredMonsters = this.state.monsters.filter((monster) => {
+    const filteredMonsters = monsters.filter((monster) => {
       // returns boolean
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -79,21 +95,7 @@ class App extends Component {
           className='search-box'
           type='search'
           placeholder='search monsters'
-          onChange={(event) => {
-            // console.log({ startingArray: this.state.monsters });
-            // lowerCase the search
-            const searchField = event.target.value.toLocaleLowerCase();
-
-            // updates our state
-            this.setState(
-              () => {
-                return { searchField };
-              },
-              () => {
-                // console.log({ endingArray: this.state.monsters });
-              }
-            );
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
